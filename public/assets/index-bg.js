10e8b793bc93a1813b8279071f3af21349a1e2ad
ConstantinosMas <mastrapasconstantinos@yahoo.gr>
2023-03-08 19:21:42 +0200

    add more background images change code for easier background image number adjustment

--- public/assets/index-bg.js
+++ public/assets/index-bg.js
@@ -2,18 +2,20 @@ $('.navbar').removeClass('bg-white');
 $('.navbar').addClass('bg-transparent');
 
 let current_nr = 1
-    const img_loc = ["url('/public/assets/img/landing/1.jpg')", "url('/public/assets/img/landing/2.jpg')",
-                     "url('/public/assets/img/landing/3.jpg')", "url('/public/assets/img/landing/4.jpg')",
-                     "url('/public/assets/img/landing/5.jpg')", "url('/public/assets/img/landing/6.jpg')"];
+let img_loc = []
+for (let i = 1; i <= 8; i++) { //the maximum number of i needs to be the same number of pictures in the img/landing folder
+    let pic_url = `url('/public/assets/img/landing/${i}.jpg')`;
+    img_loc.push(pic_url);
+}
 
-    $(document).on('click', 'body', function() {
-        current_nr++;
-        if (current_nr > 5) {
-            current_nr = 1;
-        } 
-        console.log(current_nr);   
-        setTimeout(function() {
-            $("body").css("background-image", img_loc[current_nr - 1]);
-                  }, 3000)
-        
-    })
+$(document).on('click', 'body', function() {
+    current_nr++;
+    if (current_nr > 8) {
+        current_nr = 1;
+    } 
+    console.log(current_nr);   
+    setTimeout(function() {
+        $("body").css("background-image", img_loc[current_nr - 1]);
+                }, 3000)
+    
+})
